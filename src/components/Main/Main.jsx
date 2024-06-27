7934cbb3e937204fffc95657ab6ab33e94cac16a
Amadeo Bon <amadeosph@outlook.com>
2024-06-27 02:25:04 -0300

    add context and config

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,68 +1,139 @@
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
 import "./style.css";
+import { Context } from "../../Context/Context";
 
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
   return (
-    <div className="main flex-1 min-h-screen pb-[15vh] relative">
+    <div className="main flex-1 min-h-screen pb-[15vh] relative overflow-x-hidden">
       <div className="nav flex items-center justify-between text-2xl p-5 text-[#585858]">
         <p>Gemini AI</p>
         <img
-          className="rounded-full w-10"
+          className="w-10 rounded-full"
           src={assets.user_icon}
           alt="user icon"
         />
       </div>
       <div className="main-container max-w-[900px] m-auto">
-        <div className="greet mx-[50px] my-0 text-[56px] text-[#c4c7c5] font-[500] p-5">
-          <p className="span">
-            <span className="">Hello, Dev.</span>
-          </p>
-          <p>How can i help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">Suggest beautiful places to see on an upcoming road trip.</p>
-            <img
-              className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
-              src={assets.compass_icon}
-              alt="compass icon"
-            />
-          </div>
-          <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">Briefly summarize this concept: urban planning.</p>
-            <img
-              className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
-              src={assets.bulb_icon}
-              alt="compass icon"
-            />
-          </div>
-          <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">Brainstorm team bonding activities for our work retreat.</p>
-            <img
-              className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
-              src={assets.message_icon}
-              alt="compass icon"
-            />
-          </div>
-          <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">Improve the readability of the following code.</p>
-            <img
-              className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
-              src={assets.code_icon}
-              alt="compass icon"
-            />
-          </div>
-        </div>
-        <div className="main-bottom absolute bottom-0 w-full max-w-[900px] py-5 m-auto">
-            <div className="search-box flex items-center justify-between gap-5 bg-[#f0f4f9] px-3 py-5 rounded-[50px]">
-                <input type="text" placeholder="Enter a prompt here" className="flex-1 bg-transparent border-none outline-none p-2 text-[18px]" />
-                <div className="flex items-center gap-4">
-                    <img className="w-6 cursor-pointer" src={assets.gallery_icon} alt="" />
-                    <img className="w-6 cursor-pointer" src={assets.mic_icon} alt="" />
-                    <img className="w-6 cursor-pointer" src={assets.send_icon} alt="" />
-                </div>
+        {!showResult ? (
+          <>
+            <div className="greet mx-[50px] my-0 text-[56px] text-[#c4c7c5] font-[500] p-5">
+              <p className="span">
+                <span className="text-lg md:text-4xl">Hello, Dev.</span>
+              </p>
+              <p className="text-lg md:text-4xl">How can i help you today?</p>
             </div>
-            <p className="bottom-info text-[13px] my-3 text-center font-[300]">Gemini may display incorrect information, including information about people, so be sure to confirm your answers.</p>
+            <div className="mb-16 md:mb-0 cards">
+              <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Suggest beautiful places to see on an upcoming road trip.
+                </p>
+                <img
+                  className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
+                  src={assets.compass_icon}
+                  alt="compass icon"
+                />
+              </div>
+              <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Briefly summarize this concept: urban planning.
+                </p>
+                <img
+                  className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
+                  src={assets.bulb_icon}
+                  alt="compass icon"
+                />
+              </div>
+              <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Brainstorm team bonding activities for our work retreat.
+                </p>
+                <img
+                  className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
+                  src={assets.message_icon}
+                  alt="compass icon"
+                />
+              </div>
+              <div className="card h-[200px] p-4 bg-[#f0f4f9] rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+                <p className="text-[#585858] text-[17px]">
+                  Improve the readability of the following code.
+                </p>
+                <img
+                  className="w-9 p-1 absolute bg-[#fff] rounded-[20px] bottom-3 right-3"
+                  src={assets.code_icon}
+                  alt="compass icon"
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="px-4 max-h-[70vh] overflow-y-scroll result">
+            <div className="flex items-center gap-5 my-10 result-title bg-[#edf1f6] p-2 rounded-lg">
+              <img className="w-10" src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="flex items-start gap-5 p-2 rounded-lg result-data">
+              <img className="w-10" src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p
+                  className="text-[17px] font-light leading-7"
+                  dangerouslySetInnerHTML={{ __html: resultData }}
+                ></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom absolute bottom-0 w-full max-w-[900px] min-w-[300px] py-5 px-2 m-auto mt-4">
+          <div className="search-box flex items-center justify-between gap-5 bg-[#f0f4f9] px-3 py-2 rounded-[50px]">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+              className="flex-0 w-[150px] md:w-full md:flex-1 bg-transparent border-none outline-none p-2 md:text-[18px] text-sm"
+            />
+            <div className="flex items-center gap-2 md:gap-4">
+              <img
+                className="w-5 cursor-pointer hover:scale-105"
+                src={assets.gallery_icon}
+                alt=""
+              />
+              <img
+                className="w-5 cursor-pointer hover:scale-105"
+                src={assets.mic_icon}
+                alt=""
+              />
+              {input ? (
+                <img
+                  onClick={() => onSent()}
+                  className="w-5 cursor-pointer hover:scale-105"
+                  src={assets.send_icon}
+                  alt=""
+                />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info text-[13px] my-3 text-center font-[300]">
+            Gemini may display incorrect information, including information
+            about people, so be sure to confirm your answers.
+          </p>
         </div>
       </div>
     </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,47 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
+import { Context } from "../../Context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
-    <div className="sidebar min-h-screen p-6 py-4 flex flex-col justify-between bg-[#f0f4f9]">
+    <div className="sidebar hidden min-h-screen p-6 py-4 md:flex flex-col justify-between bg-[#f0f4f9]">
       <div className="top">
         <img
-          onClick={()=>setExtended(prev=>!prev)}
-          className="menu block ml-3 w-5 cursor-pointer"
+          onClick={() => setExtended((prev) => !prev)}
+          className="block w-5 ml-3 cursor-pointer menu"
           src={assets.menu_icon}
           alt="menu icon"
         />
-        <div className="new-chat mt-12 inline-flex items-center gap-3 px-3 py-4 bg-[#e6eaf1] rounded-full text-sm text-gray-500 cursor-pointer">
+        <div onClick={()=>newChat()} className="new-chat mt-12 inline-flex items-center gap-3 px-3 py-4 bg-[#e6eaf1] rounded-full text-sm text-gray-500 cursor-pointer">
           <img className="w-5" src={assets.plus_icon} alt="plus icon" />
           {extended ? <p className="font-medium">New Chat</p> : null}
         </div>
         {extended ? (
-          <div className="recent flex flex-col">
-            <p className="recent-title mt-8 mb-5 p-3">Recent</p>
-            <div className="recent-entry flex items-center gap-3 p-3 pr-10 rounded-full text-gray-700 cursor-pointer hover:bg-[#e2e6eb]">
-              <img
-                className="w-5"
-                src={assets.message_icon}
-                alt="message icon"
-              />
-              <p>What is ReactJs ...</p>
-            </div>
+          <div className="flex flex-col recent">
+            <p className="p-3 mt-8 mb-5 recent-title">Recent</p>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  onClick={()=>loadPrompt(item)}
+                  className="recent-entry flex items-center gap-3 p-3 pr-10 rounded-full text-gray-700 cursor-pointer hover:bg-[#e2e6eb]"
+                >
+                  <img
+                    className="w-5"
+                    src={assets.message_icon}
+                    alt="message icon"
+                  />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
